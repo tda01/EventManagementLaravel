7a7670e68a79621b704f9afc4b1d826f6a7e77de
Dani <trifdaniel01@gmail.com>
2023-12-14 01:41:44 +0200

    Fixed background bug

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -17,29 +17,90 @@
                     </div>
                 @endif
                 {{ Form::open(array('route' => 'events.store','method'=>'POST')) }}
-                <div class="mt-3">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="title" class="form-label">Title</label>
-                        <input type="text" class="form-control" id="title" name="title">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Event Details</h5>
+                            <div class="mt-3">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="title" class="form-label">Title</label>
+                                    <input type="text" class="form-control" id="title" name="title">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="description" class="form-label">Description</label>
+                                    <textarea class="form-control" name="description" id="description" cols="30" rows="5"></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="location" class="form-label">Location</label>
+                                    <input type="text" class="form-control" id="location" name="location">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="img" class="form-label">Img</label>
+                                    <input type="text" class="form-control" id="img" name="img">
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="description" class="form-label">Description</label>
-                        <br>
-                        <textarea class="form-control" name="description" id="description" cols="30" rows="5"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <label for="location" class="form-label">Location</label>
-                        <input type="text" class="form-control" id="location" name="location">
-                    </div>
-                    <div class="mb-3">
-                        <label for="img" class="form-label">Img</label>
-                        <input type="text" class="form-control" id="img" name="img">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Add Event</button>
-                    <a href="{{ route('events.index') }}" class="btn btn-default">Cancel</a>
 
-                </div>
+                    <div class="card mt-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Contact</h5>
+                            <div class="mt-3">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="firstName" class="form-label">First Name</label>
+                                    <input type="text" class="form-control" id="firstName" name="firstName">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="lastName" class="form-label">Last Name</label>
+                                    <input type="text" class="form-control" id="lastName" name="lastName">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="phoneNumber" class="form-label">Phone</label>
+                                    <input type="text" class="form-control" id="phoneNumber" name="phoneNumber">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mt-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Speakers</h5>
+                            <label for="speaker" class="form-label">Select Speakers</label>
+                            <select class="form-select" id="speaker" name="speaker[]" multiple>
+                                <option disabled>Select Speakers</option>
+                                @foreach ($speakers as $speaker)
+                                    <option value="{{ $speaker->id }}">{{ $speaker->firstName }} {{ $speaker->lastName }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="card mt-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Partners</h5>
+                            <div class="mb-3">
+                                <label for="partner" class="form-label">Select Partners</label>
+                                <select class="form-select" id="partner" name="partner[]" multiple>
+                                    <option disabled>Select Partners</option>
+                                    @foreach ($partners as $partner)
+                                        <option value="{{ $partner->id }}">{{ $partner->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="mt-3">
+                        <button type="submit" class="btn btn-primary">Add Event</button>
+                        <a href="{{ route('events.index') }}" class="btn btn-default">Cancel</a>
+                    </div>
                 {{ Form::close() }}
 
             </div>
